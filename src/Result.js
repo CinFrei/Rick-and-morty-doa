72ea764f8a2a4245bf0fc315e17e285d78abe111
Julian Blohm <julianscript@outlook.de>
2020-10-31 16:39:46 +0100

    Add Information slide and redo Buttonlayout

--- src/Result.js
+++ src/Result.js
@@ -3,16 +3,30 @@ import styled from 'styled-components'
 export default function Result({ name, status, location }) {
 
     const ResultStyled = styled.div`
+    display: flex;
+    flex-wrap: nowrap;
+    overflow-x: scroll;
+    scroll-snap-type: x mandatory;
+    width: 100%;
+    height: 10%;
     text-align: center;
-
-    h3 {
-        margin: 10px 0;
+    scrollbar-width: none;
+    
+    .box{
+      flex: 1 0 100%;
+      scroll-snap-align: start;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      color: white;
     }
 `
 
     return (
     <ResultStyled>
-      <h3>{name + ' is ' + status}</h3>
+      <div className="box" >{name + ' is ' + status.toLowerCase() }&rarr;</div>
+      <div className="box" >Last seen in episode</div>
+      <div className="box" >&larr;Location</div>
     </ResultStyled>
   )
 }
